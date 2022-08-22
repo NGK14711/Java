f56594c7b4fee7f93b5154547d592ded027f4a69
Gopala Krishna N <gopalan@aditiconsulting.com>
2022-08-22 12:01:04 +0530

    chapter-2

---
+++ Tapedeck.java
@@ -0,0 +1,21 @@
+public class Tapedeck {
+    boolean canRecord = false;
+    void playTape() {
+        System.out.println("tape playing");
+    }
+    void recordTape() {
+        System.out.println("tape recording");
+    }
+}
+class TapeDeckTestDrive {
+    public static void main(String [] args) {
+
+        Tapedeck t = new Tapedeck();
+        t.canRecord = true;
+        t.playTape();
+        if (t.canRecord == true) {
+            t.recordTape();
+        }
+    }
+}
+
